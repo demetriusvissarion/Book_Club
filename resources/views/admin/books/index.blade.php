79ec3ce87c9941102410b80fc81c5f5a22c026fc
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-06-02 11:59:58 +0100

    cleaned create book page

--- resources/views/admin/books/index.blade.php
+++ resources/views/admin/books/index.blade.php
@@ -1,38 +1,61 @@
 <x-layout>
     <x-setting heading="Manage Books">
-        <div class="flex flex-col">
+        <div class="flex flex-row">
+            <aside class="w-48 flex-shrink-0">
+                <h4 class="font-semibold mb-4">Links</h4>
+
+                <ul>
+                    {{-- <li>
+                        <a href="/admin/uploads" class="{{ request()->is('admin/uploads') ? 'text-blue-500' : '' }}">Book
+                            Upload
+                            Validation</a>
+                    </li> --}}
+
+                    <li>
+                        <a href="/admin/categories"
+                            class="{{ request()->is('admin/categories') ? 'text-blue-500' : '' }}">Book
+                            Categories</a>
+                    </li>
+
+                    <li>
+                        <a href="/admin/users" class="{{ request()->is('admin/users') ? 'text-blue-500' : '' }}">Users
+                            Management</a>
+                    </li>
+                </ul>
+            </aside>
             <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+
                 <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                     <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
                         <table class="min-w-full divide-y divide-gray-200">
                             <tbody class="bg-white divide-y divide-gray-200">
-                            @foreach ($books as $book)
-                                <tr>
-                                    <td class="px-6 py-4 whitespace-nowrap">
-                                        <div class="flex items-center">
-                                            <div class="text-sm font-medium text-gray-900">
-                                                <a href="/books/{{ $book->slug }}">
-                                                    {{ 'Book Title: ' . $book->title }}
-                                                </a>
+                                @foreach ($books as $book)
+                                    <tr>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="flex items-center">
+                                                <div class="text-sm font-medium text-gray-900">
+                                                    <a href="/books/{{ $book->slug }}">
+                                                        {{ 'Book Title: ' . $book->title }}
+                                                    </a>
+                                                </div>
                                             </div>
-                                        </div>
-                                    </td>
+                                        </td>
 
-                                    <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                        <a href="/admin/books/{{ $book->id }}/edit"
-                                           class="text-blue-500 hover:text-blue-600">Edit</a>
-                                    </td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                            <a href="/admin/books/{{ $book->id }}/edit"
+                                                class="text-blue-500 hover:text-blue-600">Edit</a>
+                                        </td>
 
-                                    <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                        <form method="POST" action="/admin/books/{{ $book->id }}">
-                                            @csrf
-                                            @method('DELETE')
+                                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                            <form method="POST" action="/admin/books/{{ $book->id }}">
+                                                @csrf
+                                                @method('DELETE')
 
-                                            <button class="text-xs text-gray-400">Delete</button>
-                                        </form>
-                                    </td>
-                                </tr>
-                            @endforeach
+                                                <button class="text-xs text-gray-400">Delete</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
                             </tbody>
                         </table>
                     </div>
--- resources/views/components/setting.blade.php
+++ resources/views/components/setting.blade.php
@@ -6,26 +6,7 @@
     </h1>
 
     <div class="flex">
-        <aside class="w-48 flex-shrink-0">
-            <h4 class="font-semibold mb-4">Links</h4>
 
-            <ul>
-                <li>
-                    <a href="/admin/uploads"
-                       class="{{ request()->is('admin/uploads') ? 'text-blue-500' : '' }}">Book Upload Validation</a>
-                </li>
-
-                <li>
-                    <a href="/admin/categories"
-                       class="{{ request()->is('admin/categories') ? 'text-blue-500' : '' }}">Book Categories</a>
-                </li>
-
-                <li>
-                    <a href="/admin/users"
-                       class="{{ request()->is('admin/users') ? 'text-blue-500' : '' }}">Users Management</a>
-                </li>
-            </ul>
-        </aside>
 
         <main class=" flex-1">
             <x-panel>
