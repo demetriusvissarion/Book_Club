8b27888c8ad03364fbc69e687fcf5c10c939457e
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-06-07 15:20:11 +0100

    moved author field under category

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -5,26 +5,12 @@
                 <div class="col-span-4 lg:text-center lg:pt-14 mb-10">
                     <img src="{{ asset('storage/' . $book->thumbnail) }}" alt="Book Thumbnail" class="rounded-xl">
 
-                    <p class="mt-4 block text-gray-400 text-xs">
-                        Published
-                        <time>{{ $book->created_at->diffForHumans() }}</time>
-                    </p>
-
-                    <div class="flex items-center lg:justify-center text-sm mt-4">
-                        <img src="https://i.pravatar.cc/60?u={{ auth()->id() }}" alt=""
-                            class="rounded-full h-10 w-10 object-cover">
-                        <div class="ml-3 text-left">
-                            <h5 class="font-bold">
-                                <a href="/?author={{ $book->author->username }}">{{ $book->author->name }}</a>
-                            </h5>
-                        </div>
-                    </div>
                 </div>
 
                 <div class="col-span-8">
-                    <div class="hidden lg:flex justify-between mb-2">
+                    <div class="hidden lg:flex mb-2">
                         <a href="/"
-                            class="transition-colors duration-300 relative inline-flex items-center text-lg hover:text-blue-500">
+                            class="transition-colors duration-300 relative inline-flex items-center text-lg hover:text-blue-500 z-0">
                             <svg width="22" height="22" viewBox="0 0 22 22" class="mr-2">
                                 <g fill="none" fill-rule="evenodd">
                                     <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
@@ -35,7 +21,7 @@
                                 </g>
                             </svg>
 
-                            Back to Books
+                            <p>Back to Books</p>
                         </a>
 
                         {{-- <div class="space-x-2">
@@ -50,9 +36,25 @@
                         </h1>
                     </div>
 
+                    <p class="mt-4 mb-2 block text-gray-400 text-xs">
+                        Published
+                        <time>{{ $book->created_at->diffForHumans() }}</time>
+                    </p>
+
+
                     <div class="space-x-2 mb-2">
                         <strong>Category:</strong>
                         <x-category-button :category="$book->category" />
+                    </div>
+
+                    <div class="flex items-center text-sm mt-4 mb-2">
+                        <img src="https://i.pravatar.cc/60?u={{ auth()->id() }}" alt=""
+                            class="rounded-full h-10 w-10 object-cover">
+                        <div class="ml-3 text-left">
+                            <h5 class="font-bold">
+                                <a href="/?author={{ $book->author->username }}">{{ $book->author->name }}</a>
+                            </h5>
+                        </div>
                     </div>
 
                     <strong>Description:</strong>
