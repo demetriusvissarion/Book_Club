34c7acfb354fc0a6d8b626741a4622b2d66c2e3f
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-06-07 10:42:39 +0100

    removed old mailchimp

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,34 +4,14 @@ namespace App\Providers;
 
 use App\Models\Book;
 use App\Models\User;
-// use App\Services\Newsletter;
-// use App\Services\MailchimpNewsletter;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
-// use MailchimpMarketing\ApiClient;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    // public function register(): void
-    // {
-    //     app()->bind(Newsletter::class, function () {
-    //         $client = (new ApiClient)->setConfig([
-    //             'apiKey' => config('services.mailchimp.key'),
-    //             'server' => 'us21'
-    //         ]);
 
-    //         return new MailchimpNewsletter($client);
-    //     });
-    // }
-
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         Model::unguard();
