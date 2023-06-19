1f93be92790533d340277d1afaa63eaa2b169878
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-06-19 16:03:14 +0100

    installed vapor

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit2960a3ab7863ebdc76a16e155bfc8072
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit2960a3ab7863ebdc76a16e155bfc8072', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit2960a3ab7863ebdc76a16e155bfc8072', 'loadClassLoader'));
