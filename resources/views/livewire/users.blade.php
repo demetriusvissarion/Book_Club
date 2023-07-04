944025a5c50556542b98935243c268667b20ba63
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-07-04 09:57:47 +0100

    added sorting icons

--- resources/views/livewire/users.blade.php
+++ resources/views/livewire/users.blade.php
@@ -31,14 +31,42 @@
                     <th class="px-4 py-2">
                         <div class="flex items-center">
                             <button wire:click="sortby('name')" class="font-bold">Name</button>
-                            <span>icon</span>
+                            @if ($sortField !== 'name')
+                                <span></span>
+                            @elseif($sortAsc)
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="ml-2 w-3">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M4.5 15.75l7.5-7.5 7.5 7.5" />
+                                </svg>
+                            @else
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="ml-2 w-3">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                </svg>
+                            @endif
                         </div>
                     </th>
 
                     <th class="px-4 py-2">
                         <div class="flex items-center">
                             <button wire:click="sortby('email')" class="font-bold">Email</button>
-                            <span>icon</span>
+                            @if ($sortField !== 'email')
+                                <span></span>
+                            @elseif($sortAsc)
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="ml-2 w-3">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M4.5 15.75l7.5-7.5 7.5 7.5" />
+                                </svg>
+                            @else
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="ml-2 w-3">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                </svg>
+                            @endif
                         </div>
                     </th>
 
